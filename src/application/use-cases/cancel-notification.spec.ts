706b732b4846da2421bde63ea7ffd3004f3028aa
Matheus Ramos <devmatheusr@gmail.com>
2022-12-17 11:33:42 -0300

    Get notifications, count notifications

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -1,18 +1,16 @@
 import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './erros/notification-not-found';
 
 describe('Cancel notification', () => {
-  it('should be able to Cancel notification', async () => {
+  it('should be able to a Cancel notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
-    const notification = new Notification({
-        category: 'social',
-        content: new Content("Nova solicitação de amizade"),
-        recipientId: 'example-recipient-id',
-    })
+    const notification = makeNotification();
 
     await notificationsRepository.create(notification);
     
@@ -22,4 +20,15 @@ describe('Cancel notification', () => {
 
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(expect.any(Date));
   });
-});
+
+    it('should not be able to cancel a non existing notification', async ()=> {
+      const notificationsRepository = new InMemoryNotificationsRepository();
+      const cancelNotification = new CancelNotification(notificationsRepository);
+
+      expect(() => {
+        return cancelNotification.execute({
+          notificationId: 'fake-notification-id',
+        });
+      }).rejects.toThrow(NotificationNotFound)
+        })
+    });
